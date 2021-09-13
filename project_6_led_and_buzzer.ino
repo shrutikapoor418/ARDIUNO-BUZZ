869cf93bf91ff64151dfe38db35a084ce653a723
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2021-09-13 09:32:24 +0530

    Add files via upload

---
+++ project_6_led_and_buzzer.ino
@@ -0,0 +1,30 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite(5,HIGH);
+delay(500);
+
+digitalWrite(5,LOW);
+digitalWrite(4,HIGH);
+delay(500);
+digitalWrite(4,LOW);
+digitalWrite(3,HIGH);
+delay(500);
+digitalWrite(3,LOW);
+digitalWrite(2,HIGH);
+delay(500);
+digitalWrite(2,LOW);
+digitalWrite(6,HIGH);
+delay(1000);
+digitalWrite(6,LOW);
+delay(100);
+}
